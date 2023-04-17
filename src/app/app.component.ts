cfcfbf0de85bee49ae63337201a98fb1cc5a06f3
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-17 15:11:34 +0200

    Create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'controlValueAccessor';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      name: ['', [Validators.required,  Validators.minLength(4)]],
+      last_name: ['', [Validators.required,  Validators.minLength(4)]],
+      // description: ['', [Validators.required]],
+      stock: [100, [Validators.required]],
+      password: [],
+      // age: ['', [Validators.required]],
+      // start: [new Date().toISOString().split('T')[0], [Validators.required]],
+      // end: [new Date().toISOString().split('T')[0], [Validators.required]],
+    });
+  }
+
+  saveForm(event: Event) {
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { StockComponent } from './components/role/stock.component';
+import { MatInputModule } from '@angular/material/input';
+import { PasswordComponent } from './components/password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
